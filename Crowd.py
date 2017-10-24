5e1de9c1f88d6f3ef3a6869adfc2c2dcc3b6303b
AlexisDeslandes <deslandes.alexis@live.fr>
2017-10-24 08:48:53 +0100

    refactor

--- Crowd.py
+++ Crowd.py
@@ -8,26 +8,28 @@ import sys
 from random import randint
 import time
 
+
 def move(thread_id, persons, obstacles, draw):
     while True:
         person = persons[thread_id]
         if person.y == 0:
-            if not isInObstacle(person.x-1, 0, obstacles):
+            if not isInObstacle(person.x - 1, 0, obstacles):
                 person.x -= 1
         elif person.x == 0:
-            if not isInObstacle(0, person.y-1, obstacles):
+            if not isInObstacle(0, person.y - 1, obstacles):
                 person.y -= 1
         else:
-            if isInObstacle(person.x-1, person.y-1, obstacles):
-                if not isInObstacle(person.x, person.y-1, obstacles):
+            if isInObstacle(person.x - 1, person.y - 1, obstacles):
+                if not isInObstacle(person.x, person.y - 1, obstacles):
                     person.y -= 1
-                elif not isInObstacle(person.x-1, person.y, obstacles):
+                elif not isInObstacle(person.x - 1, person.y, obstacles):
                     person.x -= 1
             else:
                 person.y -= 1
                 person.x -= 1
         if draw != 0:
             draw.update(person)
+
 
 def simulation(settings):
     nbPersons = pow(2, int(settings.persons))
@@ -41,17 +43,15 @@ def simulation(settings):
     else:
         draw = 0
 
-
     if settings.mode == "0":
         for i in range(nbPersons):
-            threads.append(_thread.start_new_thread(move, (i,persons,obstacles,draw,)))
+            threads.append(_thread.start_new_thread(move, (i, persons, obstacles, draw,)))
     elif settings.mode == 1:
         for i in range(4):
             # TODO
             threads.append(_thread.start_new_thread())
             print("TODO")
     draw.start()
-
 
     # After the simulation is over
     execTime = time.clock() - startingTime
@@ -67,6 +67,7 @@ def isInObstacle(x, y, obstacles):
                 return True
     return False
 
+
 def createObstacles():
     obstacle1 = Obstacle(50, 30, 100, 60)
     obstacle2 = Obstacle(100, 90, 105, 110)
@@ -75,14 +76,15 @@ def createObstacles():
     obstacle5 = Obstacle(300, 40, 400, 115)
     return [obstacle1, obstacle2, obstacle3, obstacle4, obstacle5]
 
+
 def createPersons(nbPersons, obstacles):
     persons = []
     spots = []
     for y in range(128):
         for x in range(512):
-                spots.append([x, y])
+            spots.append([x, y])
 
-    #remove exit as an availible spot
+    # remove exit as an availible spot
     spots.remove([0, 0])
     spots.remove([0, 1])
     spots.remove([1, 0])
@@ -96,11 +98,12 @@ def createPersons(nbPersons, obstacles):
             index = randint(0, len(spots))
             newX = spots[index][0]
             newY = spots[index][1]
-        person = Person(newX, newY, n+1)
+        person = Person(newX, newY, n + 1)
         del spots[index]
         persons.append(person)
 
     return persons
+
 
 def generateSettings():
     t, p, m = 0, 4, False
@@ -123,7 +126,7 @@ if settings.metrics:
         execTimes.append(simulation(settings))
     execTimes.remove(max(execTimes))
     execTimes.remove(min(execTimes))
-    average = (execTimes[0]+execTimes[1]+execTimes[2])/3
+    average = (execTimes[0] + execTimes[1] + execTimes[2]) / 3
     print("Execution time: " + str(average)[0:8] + "s")
 else:
     simulation(settings)
--- GroundDraw.py
+++ GroundDraw.py
@@ -1,17 +1,15 @@
-import thread
 from tkinter import *
-import _thread
 
 
 class GroundDraw:
     def __init__(self, obstacles, persons):
         self.factor = 3
         self.window = Tk()
-        self.createCanvas()
-        self.drawObstacles(obstacles)
+        self.canvas = self.create_canvas()
+        self.draw_obstacles(obstacles)
         self.characterPosition = {}
-        self.drawPersons(persons)
-        self.drawExit()
+        self.draw_persons(persons)
+        self.draw_exit()
         self.canvas.pack()
 
     def update(self, person):
@@ -23,18 +21,18 @@ class GroundDraw:
                                                fill='red')
         self.characterPosition[person.id] = newRect
 
-    def createCanvas(self):
+    def create_canvas(self):
         longeur = 512 * self.factor
         largeur = 128 * self.factor
-        self.canvas = Canvas(self.window, width=longeur, height=largeur, background='gray')
+        return Canvas(self.window, width=longeur, height=largeur, background='gray')
 
-    def drawObstacles(self, obstacles):
+    def draw_obstacles(self, obstacles):
         for i in range(0, len(obstacles), 1):
             obstacle = obstacles[i]
             self.canvas.create_rectangle(obstacle.x1 * self.factor, obstacle.y1 * self.factor,
                                          obstacle.x2 * self.factor, obstacle.y2 * self.factor, fill='black')
 
-    def drawPersons(self, persons):
+    def draw_persons(self, persons):
         for i in range(0, len(persons), 1):
             person = persons[i]
             x = person.x
@@ -44,7 +42,7 @@ class GroundDraw:
                                                    fill='red')
             self.characterPosition[person.id] = newRect
 
-    def drawExit(self):
+    def draw_exit(self):
         self.canvas.create_rectangle(0, 0, 6, 6, fill='green')
 
     def start(self):
